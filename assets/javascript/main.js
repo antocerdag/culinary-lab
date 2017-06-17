34b7724f0625868567cb00170adb77dd0f512346
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-17 17:02:05 -0400

    navbar scroll listo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,15 @@
-main.js
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#navbar');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar").css('background-color', '#000000');
+       }else {
+         $('.navbar').css('background-color', 'transparent');
+      }
+   });
+    }
+});
